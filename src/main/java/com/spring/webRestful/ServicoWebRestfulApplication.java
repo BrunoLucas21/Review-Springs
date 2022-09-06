189b98bc521dd269a7a359ed333869123f271393
Bruno Lucas <brunolucasafreire@gmail.com>
2022-09-06 15:54:48 -0300

    construindo um serviço web restful

---
+++ src/main/java/com/spring/webRestful/ServicoWebRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.spring.webRestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoWebRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoWebRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/webRestful/ServicoWebRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.webRestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoWebRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
